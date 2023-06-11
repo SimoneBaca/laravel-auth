341473ce317c82b71b8f33a749f285be2fa24cbd
Simonebaca <simobaco99@gmail.com>
2023-06-11 18:46:22 +0200

    start many to many

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,9 @@ class DatabaseSeeder extends Seeder
         // ]);
         
         $this->call([
-            PostSeeder::class
+            PostSeeder::class,
+            CategorySeeder::class
         ]);
+       
     }
 }
